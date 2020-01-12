1e5e19fa3e3a5fa2983599a1666d105081e257fe
Deimos1994 <31592485+Deimos1994@users.noreply.github.com>
2020-01-13 01:43:37 +0200

    Update morse_sender.ino

--- morse_sender.ino
+++ morse_sender.ino
@@ -1,6 +1,7 @@
 //Morse code generator. Serhii Bazavluk 2020
 //YouTube: https://www.youtube.com/electricalpro
-//Supports letters digits and symbols
+//Demonstration: https://www.youtube.com/watch?v=Mw76lK94tRw
+//Supports letters digits and some symbols
 //You can set speed via serial command SetUnit:50
 //You can update message via serial as well
 
@@ -10,9 +11,9 @@ String morseArray[63] = {"-.-.--", "", "", "...-..-", "", ".-...", ".----.", "-.
 
 
 int ledPin = 2; // pin that used to send morse code.
-bool isInverted = false; // build-in LED is inverted on esp8266
-int oneUnitDelay = 80; // 250 is good to decode with smartphone camera(try 80 - 300, lower = faster)
-String DefaultTextToSend = "Hello world, I'm Morse code beacon";
+bool isInverted = false; // build-in LED is inverted on esp8266 so it needs to be 'true' if you just blinking using build-in LED
+int oneUnitDelay = 80; // it is dot time in ms. 250 is good to decode with smartphone camera (try 80 - 300, lower = faster speed)
+String DefaultTextToSend = "Hello world, I'm Morse code beacon"; // text to send in a loop
 
 void setup() {
   Serial.begin(115200);
@@ -25,7 +26,7 @@ void loop() {
   for(int i = 0; i <= textToSend.length(); i++)
   {
     Serial.print(textToSend[i]); // print symbols we currently sending
-    if(textToSend[i]>=33 && textToSend[i]<=95) // check for tatgeted ASCII range
+    if(textToSend[i]>=33 && textToSend[i]<=95) // check for targeted ASCII range
     {
         String morseToSend = morseArray[textToSend[i]-33]; // use morse array to covert
         for(int x = 0; x <= morseToSend.length(); x++)
@@ -38,7 +39,7 @@ void loop() {
     {
       delay(oneUnitDelay*4); //  delay for space (adding oneUnitDelay*4 delay to get oneUnitDelay*7 in total)
     }
-  serialHandle(); // handle serial
+  serialHandle(); // handle serial, remove this if not needed
   }
 delay(oneUnitDelay*28); // between messages delay
 Serial.println(); // print on a new line
@@ -61,8 +62,8 @@ void sendDashOrDot(byte sendChar){
   }
 }
 
-void serialHandle(){
-  // Serial control part
+void serialHandle(){ // remove this function if it is not needed
+  // if you type text in serial console it will be transmitted, or you can update speed by sending SetUnit:50
   if (Serial.available() > 0) //check if new date have arraved
   {
     Serial.println(" ");
